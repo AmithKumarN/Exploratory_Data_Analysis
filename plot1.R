970ab82ffdebfe1ebb1085f7371b564301787773
Amith Kumar <n.amith1970@gmail.com>
2020-08-25 23:11:14 +0530

    The first 3 plots

---
+++ plot1.R
@@ -0,0 +1,6 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+totalNEI <- aggregate(Emissions ~ year, NEI, sum)
+plot(totalNEI$year, totalNEI$Emissions, type = "o", col = "steelblue3", main = expression("Total US "~ PM[2.5]~ "Emissions by Year"), ylab = expression("Total US "~   PM[2.5] ~ "Emissions"), xlab = "Year")
+dev.copy(png,"plot1.png", width=480, height=480)
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,7 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+baltimore <- subset(NEI, NEI$fips == "24510")
+totalBaltimore <- aggregate(Emissions ~ year, baltimore, sum)
+plot(totalBaltimore$year, totalBaltimore$Emissions, type = "o", main = expression("Total Baltimore" ~ PM[2.5] ~ "Emissions by Year"), xlab = "Year", ylab = expression("Total Baltimore "~ PM[2.5] ~ "Emissions"), col = "steelblue3")
+dev.copy(png,"plot2.png", width=480, height=480)
+dev.off()
